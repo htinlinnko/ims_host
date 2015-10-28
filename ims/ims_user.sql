ccc0930f8c49b025d6c0b9e1d2702c377d23f918
htinlinnko <htinlinnko.sg@gmail.com>
2015-10-28 15:05:06 +0800

    Created db project

---
+++ ims/ims_user.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ims_user]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1000, 1), 
+    [staff_id] VARCHAR(50) NULL, 
+    [user_name] VARCHAR(50) NULL, 
+    [first_name] VARCHAR(200) NULL, 
+    [middle_name] VARCHAR(200) NULL, 
+    [last_name] VARCHAR(200) NULL, 
+    [active] BIT NOT NULL, 
+    [inactive_date] DATETIME NULL, 
+    [password] VARCHAR(200) NULL
+)
